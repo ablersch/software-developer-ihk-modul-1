a5963c2d7f3ef8ad00d03110b6fdd0adae405481
ablersch <andreas@blersch.org>
2024-09-14 10:04:24 +0200

    Add excercise

---
+++ exercises/Uebung_02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp2;
+
+internal class Program
+{
+    /// <summary>
+    /// Defines the entry point of the application.
+    /// </summary>
+    private static void Main()
+    {
+        Console.WriteLine("Wert 1 eingeben:");
+        var wert1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Wert 2 eingeben:");
+        var wert2 = double.Parse(Console.ReadLine());
+
+        var summe = wert1 + wert2;
+        Console.WriteLine(summe);
+
+        bool isEqual = wert1 == wert2;
+        Console.WriteLine(isEqual);
+
+        //Console.WriteLine(wert1 == wert2);  // kürzere Variante
+    }
+}
