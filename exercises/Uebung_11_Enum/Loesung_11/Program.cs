82efc064188e4d2fa6de0dc22d839be5f6e946f9
ablersch <andreas@blersch.org>
2024-10-11 20:15:57 +0200

    Add excercise

---
+++ exercises/Uebung_11_Enum/Loesung_11/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace EnumAufgabe;
+
+internal class Program
+{
+    private enum Level
+    {
+        Beginner = 1,
+        Advanced = 2,
+        Master = 3
+    };
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcher Level besitzt der Spieler (1-3)?");
+        var input = Console.ReadLine();
+
+        bool result = int.TryParse(input, out var inputZahl);
+        if (!result)
+        {
+            Console.WriteLine("Es wurde keine Zahl eingegeben.");
+            return;
+        }
+
+        if (!Enum.IsDefined(typeof(Level), inputZahl))
+        {
+            Console.WriteLine("Es wurde kein gültiger Level eingegeben.");
+            return;
+        }
+
+        Level spielerLevel = (Level)inputZahl;
+
+        Console.WriteLine($"Der Spieler hat den Level: \"{spielerLevel}\"");
+    }
+}
---
+++ exercises/Uebung_12_Passwort/README.md
@@ -0,0 +1,21 @@
+# Übung - Passwort
+
+## Teil 1
+
+Schreibe ein Programm, das in der Konsole ein Passwort entgegennimmt und diese als * anzeigt. Die Eingabe des Passworts wird mit **Enter** beendet.
+
+### Bemerkung
+
+* Falscheingaben sollen korrigiert werden können.
+
+### Hinweis
+
+Nutzen Sie `ConsoleKeyInfo`.
+
+## Teil 2
+
+Lasse den Benutzer ein Passwort eingeben und prüfe, ob das Passwort mindestens 8 Zeichen lang ist. Gib dem Benutzer entsprechende Hinweise, wenn das Passwort zu kurz ist.
+
+## Teil 3
+
+Prüfe, ob das Passwort sowohl Buchstaben als auch Zahlen enthält.
