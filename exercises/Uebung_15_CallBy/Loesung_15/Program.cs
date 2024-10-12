75492d9519f989ff7d8a554a869324aaa71d67a0
ablersch <andreas@blersch.org>
2024-10-12 13:29:51 +0200

    Add excercise

---
+++ exercises/Uebung_15_CallBy/Loesung_15/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CallBy;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int score = 10;
+
+        UpdateScore(ref score);
+        Console.WriteLine(score);
+
+        // Wert welcher zurückgegeben wurde der Variable neu zuweisen.
+        score = UpdateScore2(score);
+        Console.WriteLine(score);
+    }
+
+    // Teil 1
+    private static void UpdateScore(ref int score)
+    {
+        score += 10;
+    }
+
+    // Teil 2
+    private static int UpdateScore2(int score)
+    {
+        // Wert zurückgeben
+        return score += 10;
+    }
+}
