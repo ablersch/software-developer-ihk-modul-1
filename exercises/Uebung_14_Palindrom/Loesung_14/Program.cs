8fdfdb3a3884d5a0be71e7c884aa32eaec8afe52
ablersch <andreas@blersch.org>
2024-10-12 13:18:43 +0200

    Add excercise

---
+++ exercises/Uebung_14_Palindrom/Loesung_14/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Palindrom;
+
+internal class Program
+{
+    /// <summary>
+    /// Prüfen ob der Input ein Palindrom ist.
+    /// </summary>
+    /// <param name="checkForPalindrom">Zu prüfender Wert.</param>
+    /// <returns>true = ist ein Palindrom; false = kein Palindrom</returns>
+    private static bool IsPalindrom(string checkForPalindrom)
+    {
+        string wortReverse = string.Empty;
+
+        // Entferne alle Leerzeichen
+        checkForPalindrom = checkForPalindrom.Replace(" ", "");
+
+        // Alles in Kleinbuchstaben
+        checkForPalindrom = checkForPalindrom.ToLower();
+
+        // String spiegeln mit einer Schleife (von vorne)
+        for (int i = 0; i < checkForPalindrom.Length; i++)
+        {
+            wortReverse += checkForPalindrom[checkForPalindrom.Length - i - 1];
+        }
+
+        // Alternative 1
+        //wortReverse = ReverseString(checkForPalindrom);
+
+        // Alternative 2
+        // String in ein Array wandeln, dieses Array spiegeln
+        // wieder in ein Array wandeln und über String Konstruktor neuen String erstellen
+        //wortReverse = new string(checkForPalindrom.ToCharArray().Reverse().ToArray());
+
+        // Kurzes If
+        return (checkForPalindrom == wortReverse) ? true : false;
+    }
+
+    private static bool IsPalindromVersion2(string checkForPalindrom)
+    {
+        // Entferne alle Leerzeichen
+        checkForPalindrom = checkForPalindrom.Replace(" ", "");
+
+        // Alles in Kleinbuchstaben
+        checkForPalindrom = checkForPalindrom.ToLower();
+
+        // Einzelne Zeichen vom Anfang und vom Ende beginnend vergleichen
+        int leftIndex = 0;
+        int rightIndex = checkForPalindrom.Length - 1;
+
+        while (leftIndex < rightIndex)
+        {
+            if (checkForPalindrom[leftIndex] != checkForPalindrom[rightIndex])
+            {
+                return false; // kein Palindorm
+            }
+            leftIndex++;
+            rightIndex--;
+        }
+
+        return true;
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Palindrom-Test");
+        Console.WriteLine("Geben sie bitte das zu pruefende Wort ein: ");
+
+        var wort = Console.ReadLine();
+
+        var isPalindrom = IsPalindrom(wort);
+
+        if (isPalindrom)
+        {
+            Console.WriteLine($"Das Wort {wort} ist ein Palindrom\n");
+        }
+        else
+        {
+            Console.WriteLine($"Das Wort {wort} ist kein Palindrom\n");
+        }
+
+        Console.ReadLine();
+    }
+
+    /// <summary>
+    /// String umdrehen mit Array
+    /// </summary>
+    /// <param name="s">String der umgedreht werden soll</param>
+    /// <returns>Gibt umgedrehten String zurück</returns>
+    private static string ReverseString(string s)
+    {
+        char[] arr = s.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
+    }
+}
---
+++ exercises/Uebung_15_CallBy/README.md
@@ -0,0 +1,17 @@
+# Übung - Call By
+
+## Aufgabe - Teil 1
+
+Erstellen Sie eine Methode `UpdateScore`, die den Punktestand eines Spielers (`int score`) übergeben bekommt und diesen mit einem neuen Wert aktualisiert. Verwenden Sie "Call by Reference".
+In der Methode soll dem Punktestand ein Wert addiert werden z.B. 10.
+
+Zeigen Sie im Hauptprogramm, dass sich der Punktestand auch außerhalb der Methode verändert hat.
+
+
+## Aufgabe - Teil 2
+
+Erstellen Sie eine Methode `UpdateScore2`, die den Punktestand eines Spielers (`int score`) übergeben bekommt und diesen mit einem neuen Wert aktualisiert. Verwenden Sie "Call by Value".
+In der Methode soll dem Punktestand ein Wert addiert werden z.B. 10.
+
+* Wird der Wert an die `Main`-Methode übergeben?
+* Wie muss die Methode `UpdateScore2` angepasst werden damit der geänderte Wert von der Methode zurück an die `Main` Methode kommt ohne "Call by Reference" zu verwenden.
